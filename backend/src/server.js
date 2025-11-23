e31da0f475606dc93b395714d94efbaecb055e37
Alec Creasy <alectmc@gmail.com>
2025-11-23 11:49:47 -0600

    Changed backend server port from 5000 -> 8080, 5000 conflicts with other processes on macOS

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,5 +18,5 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 
 // Use port in .env or 5000 by default and listen on this port.
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
--- my-react-app/src/Register.jsx
+++ my-react-app/src/Register.jsx
@@ -35,7 +35,7 @@ export default function Register() {
 
         // Send to Express server as a POST request.
         try{
-            const res = await fetch("http://localhost:5000/api/auth/register", {
+            const res = await fetch("http://localhost:8080/api/auth/register", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(form)
